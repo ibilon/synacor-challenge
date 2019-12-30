2ba43a8d52a44fa50fdd294294e2ac4f6c2cf21b
Valentin Lemière <valentin@ibilon.com>
2019-12-30 19:46:10 +0100

    Opcodes 0, 1, 2, 3, 6, 7, 8, 17, 18

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
+enum Value {
+	Literal(u16),
+	Register(u16),
+	Invalid(u16),
+}
+
 fn main() {
 	let args: Vec<String> = std::env::args().collect();
 
@@ -24,20 +30,117 @@ fn main() {
 		memory.push((data[i + 1] as u16) << 8 | (data[i] as u16));
 	}
 
-	let _registers: [u16; 8] = [0, 0, 0, 0, 0, 0, 0, 0];
-	let _stack: Vec<u16> = Vec::new();
+	let mut registers: [u16; 8] = [0, 0, 0, 0, 0, 0, 0, 0];
+	let mut stack: Vec<u16> = Vec::new();
 	let mut pc = 0;
 
-	let mut next = || {
-		let code = memory[pc];
-		pc += 1;
-		return code;
-	};
+	fn parse(word: u16) -> Value {
+		if word <= 32767 {
+			return Value::Literal(word);
+		} else if word <= 32775 {
+			return Value::Register(word - 32768);
+		} else {
+			return Value::Invalid(word);
+		}
+	}
+
+	fn get(value: Value, registers: [u16; 8]) -> u16 {
+		return match value {
+			Value::Literal(v) => v,
+			Value::Register(r) => registers[r as usize],
+			Value::Invalid(v) => panic!(format!("Using {} as value", v)),
+		};
+	}
+
+	fn set(a: Value, b: u16, mut registers: [u16; 8]) {
+		match a {
+			Value::Literal(_) => panic!("set address is literal"),
+			Value::Register(r) => registers[r as usize] = b,
+			Value::Invalid(_) => panic!("set address is invalid"),
+		}
+	}
 
 	loop {
-		match next() {
-			19 => print!("{}", next() as u8 as char),
-			21 => (),
+		match memory[pc] {
+			// halt
+			0 => break,
+
+			// set a b
+			1 => {
+				match parse(memory[pc + 1]) {
+					Value::Register(r) => {
+						registers[r as usize] = get(parse(memory[pc + 2]), registers)
+					}
+
+					_ => panic!("set on non register"),
+				}
+
+				pc += 3;
+			}
+
+			// push a
+			2 => {
+				stack.push(get(parse(memory[pc + 1]), registers));
+				pc += 2;
+			}
+
+			// pop a
+			3 => {
+				match stack.pop() {
+					None => panic!("pop but stack is empty"),
+					Some(v) => set(parse(memory[pc + 1]), v, registers),
+				};
+				pc += 2;
+			}
+
+			// jmp a
+			6 => pc = get(parse(memory[pc + 1]), registers) as usize,
+
+			// jt a b
+			7 => {
+				let a = get(parse(memory[pc + 1]), registers);
+				let b = get(parse(memory[pc + 2]), registers);
+
+				if a != 0 {
+					pc = b as usize;
+				} else {
+					pc += 3;
+				}
+			}
+
+			// jf a b
+			8 => {
+				let a = get(parse(memory[pc + 1]), registers);
+				let b = get(parse(memory[pc + 2]), registers);
+
+				if a == 0 {
+					pc = b as usize;
+				} else {
+					pc += 3;
+				}
+			}
+
+			// call a
+			17 => {
+				stack.push(pc as u16);
+				pc = get(parse(memory[pc + 1]), registers) as usize;
+			}
+
+			// ret
+			18 => match stack.pop() {
+				None => break,
+				Some(val) => pc = val as usize,
+			},
+
+			// out a
+			19 => {
+				print!("{}", memory[pc + 1] as u8 as char);
+				pc += 2;
+			}
+
+			// noop
+			21 => pc += 1,
+
 			v => panic!(format!("unknown opcode {}", v)),
 		};
 	}
