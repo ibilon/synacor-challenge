f6f9bbbf8c91a493c64d7f346c196daa40ab984c
Valentin Lemière <valentin@ibilon.com>
2019-12-30 22:24:41 +0100

    VM pass selftest, missiing input opcode

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use std::path::Path;
 enum Value {
 	Literal(u16),
 	Register(u16),
-	Invalid(u16),
 }
 
 fn main() {
@@ -23,40 +22,42 @@ fn main() {
 		.read_to_end(&mut data)
 		.expect(&format!("Couldn't read file {}", file));
 
+	//*
 	let mut memory: Vec<u16> = Vec::new();
 
 	for i in (0..data.len()).step_by(2) {
 		// stored as little endian: low high
 		memory.push((data[i + 1] as u16) << 8 | (data[i] as u16));
 	}
+	// */
+	// let memory = vec![9, 32768, 32758, 15 + '0' as u16, 19, 32768, 0];
 
 	let mut registers: [u16; 8] = [0, 0, 0, 0, 0, 0, 0, 0];
 	let mut stack: Vec<u16> = Vec::new();
 	let mut pc = 0;
 
 	fn parse(word: u16) -> Value {
-		if word <= 32767 {
-			return Value::Literal(word);
+		return if word <= 32767 {
+			Value::Literal(word)
 		} else if word <= 32775 {
-			return Value::Register(word - 32768);
+			Value::Register(word - 32768)
 		} else {
-			return Value::Invalid(word);
-		}
-	}
-
-	fn get(value: Value, registers: [u16; 8]) -> u16 {
-		return match value {
-			Value::Literal(v) => v,
-			Value::Register(r) => registers[r as usize],
-			Value::Invalid(v) => panic!(format!("Using {} as value", v)),
+			panic!(format!("Value {} is invalid", word));
 		};
 	}
 
-	fn set(a: Value, b: u16, mut registers: [u16; 8]) {
-		match a {
-			Value::Literal(_) => panic!("set address is literal"),
-			Value::Register(r) => registers[r as usize] = b,
-			Value::Invalid(_) => panic!("set address is invalid"),
+	fn get(address: usize, registers: &[u16; 8], memory: &Vec<u16>) -> u16 {
+		return match parse(memory[address]) {
+			Value::Literal(v) => v,
+			Value::Register(r) => registers[r as usize],
+		};
+	}
+
+	fn set(address: usize, value: u16, registers: &mut [u16; 8], memory: &mut Vec<u16>) {
+		match parse(memory[address]) {
+			// Value::Literal(v) => panic!(format!("set address is literal {}", v)),
+			Value::Literal(v) => memory[v as usize] = value,
+			Value::Register(r) => registers[r as usize] = value,
 		}
 	}
 
@@ -67,20 +68,16 @@ fn main() {
 
 			// set a b
 			1 => {
-				match parse(memory[pc + 1]) {
-					Value::Register(r) => {
-						registers[r as usize] = get(parse(memory[pc + 2]), registers)
-					}
+				let b = get(pc + 2, &registers, &memory);
 
-					_ => panic!("set on non register"),
-				}
+				set(pc + 1, b, &mut registers, &mut memory);
 
 				pc += 3;
 			}
 
 			// push a
 			2 => {
-				stack.push(get(parse(memory[pc + 1]), registers));
+				stack.push(get(pc + 1, &registers, &memory));
 				pc += 2;
 			}
 
@@ -88,18 +85,48 @@ fn main() {
 			3 => {
 				match stack.pop() {
 					None => panic!("pop but stack is empty"),
-					Some(v) => set(parse(memory[pc + 1]), v, registers),
+					Some(v) => set(pc + 1, v, &mut registers, &mut memory),
 				};
 				pc += 2;
 			}
 
+			// eq a b c
+			4 => {
+				let b = get(pc + 2, &registers, &memory);
+				let c = get(pc + 3, &registers, &memory);
+
+				set(
+					pc + 1,
+					if b == c { 1 } else { 0 },
+					&mut registers,
+					&mut memory,
+				);
+
+				pc += 4;
+			}
+
+			// gt a b c
+			5 => {
+				let b = get(pc + 2, &registers, &memory);
+				let c = get(pc + 3, &registers, &memory);
+
+				set(
+					pc + 1,
+					if b > c { 1 } else { 0 },
+					&mut registers,
+					&mut memory,
+				);
+
+				pc += 4;
+			}
+
 			// jmp a
-			6 => pc = get(parse(memory[pc + 1]), registers) as usize,
+			6 => pc = get(pc + 1, &registers, &memory) as usize,
 
 			// jt a b
 			7 => {
-				let a = get(parse(memory[pc + 1]), registers);
-				let b = get(parse(memory[pc + 2]), registers);
+				let a = get(pc + 1, &registers, &memory);
+				let b = get(pc + 2, &registers, &memory);
 
 				if a != 0 {
 					pc = b as usize;
@@ -110,8 +137,8 @@ fn main() {
 
 			// jf a b
 			8 => {
-				let a = get(parse(memory[pc + 1]), registers);
-				let b = get(parse(memory[pc + 2]), registers);
+				let a = get(pc + 1, &registers, &memory);
+				let b = get(pc + 2, &registers, &memory);
 
 				if a == 0 {
 					pc = b as usize;
@@ -120,10 +147,102 @@ fn main() {
 				}
 			}
 
+			// add a b c
+			9 => {
+				let b = get(pc + 2, &registers, &memory) as u32;
+				let c = get(pc + 3, &registers, &memory) as u32;
+
+				set(
+					pc + 1,
+					((b + c) % 32768) as u16,
+					&mut registers,
+					&mut memory,
+				);
+
+				pc += 4;
+			}
+
+			// mult a b c
+			10 => {
+				let b = get(pc + 2, &registers, &memory) as u32;
+				let c = get(pc + 3, &registers, &memory) as u32;
+
+				set(
+					pc + 1,
+					((b * c) % 32768) as u16,
+					&mut registers,
+					&mut memory,
+				);
+
+				pc += 4;
+			}
+
+			// mod a b c
+			11 => {
+				let b = get(pc + 2, &registers, &memory);
+				let c = get(pc + 3, &registers, &memory);
+
+				set(pc + 1, b % c, &mut registers, &mut memory);
+
+				pc += 4;
+			}
+
+			// and a b c
+			12 => {
+				let b = get(pc + 2, &registers, &memory);
+				let c = get(pc + 3, &registers, &memory);
+
+				set(pc + 1, b & c, &mut registers, &mut memory);
+
+				pc += 4;
+			}
+
+			// or a b c
+			13 => {
+				let b = get(pc + 2, &registers, &memory);
+				let c = get(pc + 3, &registers, &memory);
+
+				set(pc + 1, b | c, &mut registers, &mut memory);
+
+				pc += 4;
+			}
+
+			// not a b
+			14 => {
+				let b = get(pc + 2, &registers, &memory);
+
+				set(pc + 1, (!b) & 32767, &mut registers, &mut memory);
+
+				pc += 3;
+			}
+
+			// rmem a b
+			15 => {
+				let b = get(
+					get(pc + 2, &registers, &memory) as usize,
+					&registers,
+					&memory,
+				);
+
+				set(pc + 1, b, &mut registers, &mut memory);
+
+				pc += 3;
+			}
+
+			// wmem a b
+			16 => {
+				let a = get(pc + 1, &registers, &memory);
+				let b = get(pc + 2, &registers, &memory);
+
+				memory[a as usize] = b;
+
+				pc += 3;
+			}
+
 			// call a
 			17 => {
-				stack.push(pc as u16);
-				pc = get(parse(memory[pc + 1]), registers) as usize;
+				stack.push((pc + 2) as u16);
+				pc = get(pc + 1, &registers, &memory) as usize;
 			}
 
 			// ret
@@ -134,8 +253,13 @@ fn main() {
 
 			// out a
 			19 => {
-				print!("{}", memory[pc + 1] as u8 as char);
+				print!("{}", get(pc + 1, &registers, &memory) as u8 as char);
 				pc += 2;
+			}
+
+			// in a
+			20 => {
+				panic!("op 20 not implemented");
 			}
 
 			// noop
